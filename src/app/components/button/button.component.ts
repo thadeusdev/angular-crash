7b8d68934187c673b1ac70a4e8b0e21f9c4ca155
thadeus <thadeusnyariki94@gmail.com>
2023-08-07 12:41:26 +0300

    component input data

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,7 +6,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-  @Input() text: string;
-  @Input() color: string;
+  @Input() text!: string;
+  @Input() color!: string;
+
+  @Output() btnClick = new EventEmitter()
+
+  onClick(){
+    this.btnClick.emit();
+  }
 
 }
